79ae18168f0d70a1a9219d71665425e01314de1c
SugarBlank <eneblank19@protonmail.com>
2021-04-08 15:19:33 -0400

    Create Solution.cpp

---
+++ Merge Two Sorted Lists/Solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* result = NULL;
+            
+        if (!l1)
+            return l2;
+        if (!l2)
+            return l1;
+        
+        if(l1->val < l2->val)
+        {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        }
+        else
+        {
+            l2->next = mergeTwoLists(l2->next, l1);
+            return l2;
+        }
+        
+    }
+};
