c2f664eb98589f15976bf558cebdbed751fa9cc6
SugarBlank <eneblank19@protonmail.com>
2021-04-08 15:24:39 -0400

    Update Solution.cpp

--- Merge Two Sorted Lists/Solution.cpp
+++ Merge Two Sorted Lists/Solution.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode* result = NULL;
-            
-        if (!l1)
+  
+        if (!l1) //Doesn't equal anything
             return l2;
         if (!l2)
             return l1;
